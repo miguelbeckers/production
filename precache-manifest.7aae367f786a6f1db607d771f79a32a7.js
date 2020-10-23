9227a2530db683a4bc3ca1b353e1bef3e0107c26
miguelbeckers <52552717+miguelbeckers@users.noreply.github.com>
2020-10-23 11:52:14 -0300

    Updates

---
+++ precache-manifest.7aae367f786a6f1db607d771f79a32a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22ae5756ff5099034a4aec3e639b228",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "049931cd29eeb9fbf42c",
+    "url": "/production/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6aacf17d155b98fe79af",
+    "url": "/production/static/js/2.21374a40.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/production/static/js/2.21374a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049931cd29eeb9fbf42c",
+    "url": "/production/static/js/main.9a988a14.chunk.js"
+  },
+  {
+    "revision": "71361acf442d3089499f",
+    "url": "/production/static/js/runtime-main.5170a595.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production/static/media/logo.5d5d9eef.svg"
+  }
+]);
